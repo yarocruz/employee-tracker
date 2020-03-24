be5014f53a9a4a3e198de8bfc60524d6d17d3dff
yarocruz <yarocruz@gmail.com>
2020-03-24 10:25:49 -0400

    schema file was updated with FOREIGN Keys to connect tables by id

--- dev/employeeSchema.sql
+++ dev/employeeSchema.sql
@@ -1,25 +1,28 @@
-DROP DATABASE IF EXISTS employee_DB;
-CREATE DATABASE employee_DB;
-
 USE employee_DB;
 
-CREATE TABLE employee (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL, 
-    last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL,
-    manager_id INT
+CREATE TABLE department (
+    department_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    role_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL, 
     salary DECIMAL,
-    department_id INT
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES department(department_id)
 );
 
-CREATE TABLE department (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30) NOT NULL
+CREATE TABLE employee (
+    employee_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL, 
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES role(role_id),
+    FOREIGN KEY (manager_id) REFERENCES department(department_id)
 )
+
+
+
 
---
+++ dev/employeeSeeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO employee (first_name, last_name) VALUES ("Jerry","Seinfeld");
+INSERT INTO employee (first_name, last_name) VALUES ("George","Constanza");
+INSERT INTO employee (first_name, last_name) VALUES ("Elaine","Benes");
+INSERT INTO employee (first_name, last_name) VALUES ("Cosmo","Kramer");
+INSERT INTO employee (first_name, last_name) VALUES ("Rachel","Green");
+INSERT INTO employee (first_name, last_name) VALUES ("Phoebe","Boufay");
+INSERT INTO employee (first_name, last_name) VALUES ("Chandler","Bing");
+INSERT INTO employee (first_name, last_name) VALUES ("Joey","Tribbiani");
+INSERT INTO employee (first_name, last_name) VALUES ("Ross","Gellar");
+INSERT INTO employee (first_name, last_name) VALUES ("Monica","Gellar");
+
